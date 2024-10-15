442bfbc296c6268329a82c8d40a4ac6d38d1d5b7
Gabe Ortiz <gabe@signalnine.net>
2024-10-14 20:42:06 -0500

    Update microbit.py to add image rotation

--- microbit.py
+++ microbit.py
@@ -3,6 +3,13 @@ from microbit import *
 # Set up the serial connection with the Mac
 uart.init(baudrate=9600)
 
+def flip_image(image):
+    new = Image(5, 5)
+    for y in range(5):
+        for x in range(5):
+            new.set_pixel(x, 4 - y, image.get_pixel(x, y))
+    return new
+    
 # Main loop
 while True:
     # Check if data is available over serial
@@ -11,7 +18,8 @@ while True:
         
         if signal == b'1':
             # display a square on the onboard matrix
-            display.show(Image.SQUARE)
+            # I am flipping the image upside down because sometimes I use a different image and the wires are less messy in this orientation
+            display.show(flip_image(Image.SQUARE))
         elif signal == b'0':
             # Turn off display
             display.clear()
